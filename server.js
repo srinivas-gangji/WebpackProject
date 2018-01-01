10f77442c64530cd0e1b2ae955102170528cd8ae
sgangj200 <srinivasrao_gangji@comcast.com>
2018-01-01 00:35:42 -0700

    Updates to get ready for production

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express")
-const webpackMiddleware = require("webpack-dev-middleware")
-const webpack = require("webpack")
-const webpackConfig = require("./webpack.config")
 const app = express()
+const path = require("path")
 
-//Webpack middleware configuration
-app.use(webpackMiddleware(webpack(webpackConfig)))
+//Any additional routes go here
 
-app.listen(3050, () => {
+if (process.env.NODE_ENV != "production") {
+  const webpackMiddleware = require("webpack-dev-middleware")
+  const webpack = require("webpack")
+  const webpackConfig = require("./webpack.config")
+  //Webpack middleware configuration
+  app.use(webpackMiddleware(webpack(webpackConfig)))
+} else {
+  //'dist' is the static folder
+  app.use(express.static("dist"))
+  // Send index.html for any route (Specific for react router and browser history)
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/index.html"))
+  })
+}
+
+app.listen(process.env.PORT || 3050, () => {
   console.log("Express is running on http://localhost:3050")
 })
